c62da5eb5647837acfb59bc8e0660352208eab56
Joshua Britz <joshua@onepointfour.io>
2019-10-07 15:34:41 +0200

    Stuff

---
+++ Vue/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'never'],
+    quotes: ['error', 'single'],
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+};
